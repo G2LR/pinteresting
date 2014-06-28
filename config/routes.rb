39c4930cb22bab97934633865aa8662d70785610
G2LR <g.larochebrochard@gmail.com>
2014-06-28 17:17:14 +0200

    added devise and adaptation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
     root "pages#home"
     get "about" => "pages#about"
+    get "Sign up" => "users#sign_up"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
